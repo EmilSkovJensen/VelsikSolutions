39a05e75747352e0f6e1549a610353abd0c87992
Emil Skov Jensen <EmilJS01@gmail.com>
2024-04-08 12:45:51 +0200

    Revert "Config in python"
    This reverts commit f1f46e1245cb6cf1fb8a2d10f3f78ab1b852c7bc.

--- API/VelsikAPI/main.py
+++ API/VelsikAPI/main.py
@@ -1,6 +1,3 @@
-import uvicorn
-from configparser import ConfigParser
-
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -14,9 +11,3 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
-
-if __name__ == "__main__":
-    config = ConfigParser()
-    config.read("appsettings.ini")
-    print(config.get('Database', 'ConnectionString'))
-    uvicorn.run(app, host="127.0.0.1", port=8000)
