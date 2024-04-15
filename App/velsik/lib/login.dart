1116fd79d18d5d6f6bb02f2db8bb399a8bbb4589
EmilSkovJensen <10434636@ucn.dk>
2024-04-15 08:34:17 +0200

    new login ui

--- App/velsik/lib/login.dart
+++ App/velsik/lib/login.dart
@@ -10,54 +10,181 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final TextEditingController emailController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('UNIKABYG A/S',
-                style: TextStyle(
-                  fontSize: 24, // Change the font size as desired
-                  fontWeight: FontWeight.bold)),
-              TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  final SharedPreferences prefs = await SharedPreferences.getInstance();
-                  final AuthService authService = AuthService(prefs);
-                  await authService.signIn(emailController.text, passwordController.text);
 
-                  // Check if the user is logged in after sign-in
-                  bool isLoggedIn = await authService.isLoggedIn();
-                  if (isLoggedIn) {
-                    // Navigate to the home page
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const HomePage()),
-                    );
-                  }
-                },
-                child: const Text('Login'),
-              ),
-            ],
-          ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios,
+          size: 20,
+          color: Colors.black,),
+
+
+        ),
+      ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    const Text("Login",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                    const SizedBox(height: 20,),
+                    Text("Login to your account",
+                    style: TextStyle(
+                      fontSize: 15,
+                    color:Colors.grey[700]),)
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: <Widget>[
+                      inputFile(label: "Email", obscureText: false, controller: emailController),
+                      inputFile(label: "Password", obscureText: true, controller: passwordController)
+                    ],
+                  ),
+                ),
+                  Padding(padding:
+                  const EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                      padding: const EdgeInsets.only(top: 3, left: 3),
+                      decoration:
+                        BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          border: const Border(
+                            bottom: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black),
+                            right: BorderSide(color: Colors.black),
+
+                          )
+
+
+
+                        ),
+                      child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () async {
+                          final SharedPreferences prefs = await SharedPreferences.getInstance();
+                          final AuthService authService = AuthService(prefs);
+                          await authService.signIn(emailController.text, passwordController.text);
+
+                          // Check if the user is logged in after sign-in
+                          bool isLoggedIn = await authService.isLoggedIn();
+                          if (isLoggedIn) {
+                            // Navigate to the home page
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const HomePage()),
+                            );
+                          }
+                        },
+                        color: const Color(0xff0095FF),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+
+                        ),
+                        child: const Text(
+                          "Login", style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.white,
+
+                        ),
+                        ),
+
+                      ),
+                    ),
+                  ),
+
+
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Don't have an account?"),
+                    Text(" Sign up", style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+
+                    ),)
+                  ],
+                ),
+
+                Container(
+                  padding: const EdgeInsets.only(top: 100),
+                  height: 200,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/background.png"),
+                      fit: BoxFit.fitHeight
+                    ),
+
+                  ),
+                )
+
+              ],
+            ))
+          ],
         ),
       ),
     );
   }
+
 }
+
+
+// we will be creating a widget for text field
+Widget inputFile({label, obscureText = false, required TextEditingController controller})
+{
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: const TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.w400,
+          color:Colors.black87
+        ),
+
+      ),
+      const SizedBox(
+        height: 5,
+      ),
+      TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 0,
+          horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey.shade400
+            ),
+
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400)
+          )
+        ),
+      ),
+      const SizedBox(height: 10,)
+    ],
+  );
+}
+
