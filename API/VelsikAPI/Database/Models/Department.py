7892ade9828b84a503fe90d0f6e5c1121c9ce80a
Emil Skov Jensen <EmilJS01@gmail.com>
2024-04-19 10:51:06 +0200

    API get departments and users

---
+++ API/VelsikAPI/Database/Models/Department.py
@@ -0,0 +1,7 @@
+
+class Department:
+    def __init__(self, department_id, department_name, users):
+        self.department_id = department_id
+        self.department_name = department_name
+        self.users = users
+
