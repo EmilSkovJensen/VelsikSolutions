f500a4d380bdbeef0ce034ba6c6291e0a8446058
MathiasM9 <10407275@ucn.dk>
2024-04-30 09:30:27 +0200

    Apv quetions

--- App/velsik/lib/apv/create_apv/choose_questions.dart
+++ App/velsik/lib/apv/create_apv/choose_questions.dart
@@ -6,8 +6,9 @@ import 'package:velsik/apv/create_apv/choose_receivers.dart';
 
 class ApvQuestionPage extends StatefulWidget {
   final String? type;
+  final List<Question>? questionList;
 
-  const ApvQuestionPage({super.key, required this.type});
+  const ApvQuestionPage({super.key, required this.type, this.questionList});
 
   @override
   _ApvQuestionPageState createState() => _ApvQuestionPageState();
@@ -22,14 +23,18 @@ class _ApvQuestionPageState extends State<ApvQuestionPage> {
  @override
   void initState() {
     super.initState();
-
-    apvService.getTemplateQuestionsByTypeName(widget.type).then((questions) {
+    if(widget.questionList != null){
+      apvQuestions = widget.questionList!;
+    }else {
+        apvService.getTemplateQuestionsByTypeName(widget.type).then((questions) {
       if(questions != null){
         setState(() {
           apvQuestions = questions;
         });
       } 
     });
+    }
+    
   }
 
  @override
@@ -63,84 +68,89 @@ Widget build(BuildContext context) {
       ),
     ),
     body: Stack(
-      children: [
-        ReorderableListView(
-          buildDefaultDragHandles: true,
-          padding: const EdgeInsets.symmetric(vertical: 8.0),
-          proxyDecorator: (Widget child, int index, Animation<double> animation) {
-            return Material(
-              color: const Color(0xFF2596BE), // Set your odd/even colors here
-              elevation: 6.0, // You can change this value to match your design
-              child: child,
-            );
-          },
-          children: apvQuestions.asMap().entries.map((entry) {
-            final int index = entry.key;
-            final Question question = entry.value;
-            return ListTile(
-              key: Key(question.questionId.toString()), // Each list tile needs a unique key
-              leading: Text(
-                (index + 1).toString(),
-                style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600), // Adjust the font size as needed
-              ), // Placement number
-              title: Text(question.questionTitle, style: const TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600),
-                      ),
-              tileColor: entry.key.isOdd ? oddItemColor : evenItemColor,
-             trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => EditQuestionPage(question: question)),
-              );
-            },
-            child: Icon(Icons.edit, color: Colors.white),
-          ),
-          const SizedBox(width: 8), // Adjust the spacing as needed
-          const Icon(Icons.drag_handle, color: Colors.white),
-        ],
-      ), 
-              );
-          }).toList(),
-          onReorder: (oldIndex, newIndex) {
-            setState(() {
-              if (newIndex > oldIndex) {
-                newIndex -= 1; // Adjust index after removing the element
-              }
-              final Question question = apvQuestions.removeAt(oldIndex);
-              apvQuestions.insert(newIndex, question);
-            });
-          },
-        ),
-        Positioned(
-          bottom: 16, 
-          left: 0, 
-          right: 0, 
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Center(
-              child: GestureDetector(
-                onTap: () async {
-                   Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ApvReceiversPage(questions: apvQuestions)),
-                    );
-                },
-                child: Image.asset('assets/log-ud.png'),
+  children: [
+    Positioned.fill(
+      child: ReorderableListView(
+        buildDefaultDragHandles: true,
+        padding: const EdgeInsets.only(bottom: 140.0), // Adjust bottom padding to make space for the image
+        proxyDecorator: (Widget child, int index, Animation<double> animation) {
+          return Material(
+            color: const Color(0xFF2596BE), // Set your odd/even colors here
+            elevation: 6.0, // You can change this value to match your design
+            child: child,
+          );
+        },
+        children: apvQuestions.asMap().entries.map((entry) {
+          final int index = entry.key;
+          final Question question = entry.value;
+          return ListTile(
+            key: Key(question.questionId.toString()), // Each list tile needs a unique key
+            leading: Text(
+              (index + 1).toString(),
+              style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600), // Adjust the font size as needed
+            ), // Placement number
+            title: Text(
+              question.questionTitle,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
               ),
             ),
+            tileColor: entry.key.isOdd ? oddItemColor : evenItemColor,
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EditQuestionPage(question: question, questionList: apvQuestions)),
+                    );
+                  },
+                  child: const Icon(Icons.edit, color: Colors.white),
+                ),
+                const SizedBox(width: 8), // Adjust the spacing as needed
+                const Icon(Icons.drag_handle, color: Colors.white),
+              ],
+            ),
+          );
+        }).toList(),
+        onReorder: (oldIndex, newIndex) {
+          setState(() {
+            if (newIndex > oldIndex) {
+              newIndex -= 1; // Adjust index after removing the element
+            }
+            final Question question = apvQuestions.removeAt(oldIndex);
+            apvQuestions.insert(newIndex, question);
+          });
+        },
+      ),
+    ),
+    Positioned(
+      bottom: 16,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Center(
+          child: GestureDetector(
+            onTap: () async {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ApvReceiversPage(questions: apvQuestions)),
+              );
+            },
+            child: Image.asset('assets/log-ud.png'),
           ),
         ),
-      ],
+      ),
     ),
+  ],
+),
   );
 }
 }
